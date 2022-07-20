264c496b83ed1a22e00160e49bf1fa1831580f5e
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-19 23:29:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Pensionato_Ex_Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("---PENSIONATO---\n\n");
+
+            Console.WriteLine("Digite a quantidade de estudantes: ");
+            int quant = int.Parse(Console.ReadLine());
+
+            Quarto[] q1 = new Quarto[quant];
+
+
+            for (int i = 0; i < quant; i++)
+            {
+                Console.WriteLine("Digite o quarto a reservar: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                    while (q1[quarto] != null)
+                    {
+
+                        Console.WriteLine("JÁ RESERVADO - Digite o quarto a reservar: ");
+                        quarto = int.Parse(Console.ReadLine());
+
+                  }
+
+                Console.WriteLine("Digite o nome do aluno: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Digite o email do aluno: ");
+                string email = Console.ReadLine();
+
+
+                q1[quarto] = new Quarto
+                {
+                    Nome = nome,
+                    Email = email,
+                    Unidade = quarto
+
+                };
+
+            }
+            Console.WriteLine("----------------------");
+            for (int i = 0; i < quant; i++)
+            {
+                Console.WriteLine("Quarto " + q1[i].Unidade);
+                Console.WriteLine("----------------------");
+                Console.WriteLine(q1[i].Nome + "\n");
+                Console.WriteLine(q1[i].Email + "\n");
+                Console.WriteLine("----------------------");
+
+            }
+
+        }
+    }
+}
+
---
+++ Quartos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensionato_Ex_Vetor
+{
+    public class Quarto
+    {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Unidade { get; set; }
+
+    }
+
+   
+}
+
